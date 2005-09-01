dd3d117a5608224ea6a786bde03262a03bffb376
Klaus Guenther <thesaur@php.net>
2005-09-01 10:28:57 +0000

    -fixed bug #3948 (semicolon after case in switch) - fixed bug #3953 (errors in inline docs)
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Common/trunk@194799 c90b9560-bf6c-de11-be94-00142212c4b1

--- Common.php
+++ Common.php
@@ -201,7 +201,7 @@ class HTML_Common {
      * @param     array     $attributes   Array of attribute
      * @since     1.0
      * @access    private
-     * @return    array key
+     * @return    bool
      * @throws
      */
     function _getAttrKey($attr, $attributes)
@@ -218,7 +218,6 @@ class HTML_Common {
      * @param    array   $attr1      Original attributes array
      * @param    array   $attr2      New attributes array
      * @access   private
-     * @return   array
      */
     function _updateAttrArray(&$attr1, $attr2)
     {
@@ -334,7 +333,7 @@ class HTML_Common {
             case 'unix':
                 $this->_lineEnd = "\12";
                 break;
-            case 'mac';
+            case 'mac':
                 $this->_lineEnd = "\15";
                 break;
             default:
